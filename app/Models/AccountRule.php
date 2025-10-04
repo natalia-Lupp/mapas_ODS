d9e298758653cea21c3219dfa56eaec33b1d9123
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-10-04 18:10:02 -0300

    Corrige o nome da coluna "user_rule_id" na classe App/Models/AccountRule.

--- app/Models/AccountRule.php
+++ app/Models/AccountRule.php
@@ -14,13 +14,13 @@ class AccountRule extends Model
     protected static string $table = 'account_rules';
     protected static array $columns = [
       'user_id',
-      'rule_id'
+      'user_rule_id'
     ];
 
     public function validates(): void
     {
-        Validations::notEmpty('rule_id', $this);
-        Validations::isIdFrom('rule_id', $this, UserRule::class);
+        Validations::notEmpty('user_rule_id', $this);
+        Validations::isIdFrom('user_rule_id', $this, UserRule::class);
 
         Validations::notEmpty('user_id', $this);
         Validations::isIdFrom('user_id', $this, User::class);
