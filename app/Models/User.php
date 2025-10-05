030e04e239784e120da60a1ca5aea3ee8b8762e1
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-10-05 16:01:24 -0300

    fix: - coreige incompatibilidade do tipo de retorno de findByRuleType em UserRule - corige nome de coluna no metodo hasHule da classe User.

--- app/Models/User.php
+++ app/Models/User.php
@@ -104,7 +104,7 @@ class User extends Model
                 UserRule::class,
                 'account_rules',
                 'user_id',
-                'rule_id'
+                'user_rule_id'
             );
           /** @var UserRule[] $rules */
             $rules = $btm->get();
