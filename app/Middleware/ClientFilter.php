307a46e54effee217b7a70db02a59fbb64ec0720
Gabriel Ramos <gcrepho@gmail.com>
2025-10-05 16:02:42 -0300

    fix: altera nome da rota de redirecionamento

--- app/Middleware/ClientFilter.php
+++ app/Middleware/ClientFilter.php
@@ -8,5 +8,5 @@ class ClientFilter extends RuleMiddleware
 {
     protected static string $rule = 'client';
     protected static string $message = 'torne-se nosso cliente para acessar nosso servisso.';
-    protected static string $redirect = 'login.view';
+    protected static string $redirect = 'login';
 }
