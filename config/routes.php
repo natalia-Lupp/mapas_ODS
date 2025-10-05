878a930074be74f4e0ab0299b7904de0268dcfde
Gabriel Ramos <gcrepho@gmail.com>
2025-10-05 13:00:41 -0300

    feature: adiciona controller para gerenciar authenticação

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\AuthController;
 use Core\Router\Route;
 
 // Authentication
-Route::get('/', [HomeController::class, 'index'])->name('root');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::post('/auth/login', [AuthController::class, 'processLogin'])->name('auth.login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 
 use App\Controllers\PreviewController;
