a959218ac7734f5740b86cda81416b45ed0fcdfb
natalia-Lupp <nata.lupp@gmail.com>
2025-10-05 14:36:10 -0300

    removido rota, arquivo de encaminhamento e controler de teste de tela login

--- config/routes.php
+++ config/routes.php
@@ -5,8 +5,3 @@ use Core\Router\Route;
 
 // Authentication
 Route::get('/', [HomeController::class, 'index'])->name('root');
-
-
-use App\Controllers\PreviewController;
-
-Route::get('/preview-login', [PreviewController::class, 'login']);
--- preview-login.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Defina aqui variáveis que o login.phtml normalmente receberia
-$username = '';
-$error = '';
-
-// Inclua a view
-include __DIR__ . '/app/views/authentications/login.phtml';
