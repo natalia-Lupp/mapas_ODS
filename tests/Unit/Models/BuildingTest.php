930f32bc3354a01328763ea1d954ae65f8191619
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-10-12 20:40:27 -0300

    test: adiciona as classes de test: - tests/Unit/Models/BathroomItemTypeTest.php - tests/Unit/Models/BathroomTest.php - tests/Unit/Models/BuildingTest.php

---
+++ tests/Unit/Models/BuildingTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Bathroom;
+use App\Models\Building;
+use Tests\TestCase;
+
+class BuildingTest extends TestCase
+{
+    public function test_should_create_new_building(): void
+    {
+        $this->assertEquals(0, count(Building::all()));
+        (new Building([
+        'name' => 'H',
+        'n_floors' => 3
+        ]))->save();
+        $this->assertEquals(1, count(Building::all()));
+    }
+
+    public function test_should_has_a_validy_floor_number(): void
+    {
+        $this->assertEquals(0, count(Building::all()));
+        $building = new Building([
+        'name' => 'H',
+        'n_floors' => -3
+        ]);
+        $building->save();
+        $this->assertEquals(
+            "n_floors deve ser maior ou igual à 1!",
+            $building->errors('n_floors')
+        );
+        $this->assertEquals(0, count(Building::all()));
+    }
+
+    public function test_should_has_a_validy_name(): void
+    {
+        $this->assertEquals(0, count(Building::all()));
+        $building = new Building([
+        'name' => '',
+        'n_floors' => 3
+        ]);
+        $building->save();
+        $this->assertEquals(
+            "Deve ser maior ou igual à 1 caracteres!",
+            $building->errors('name')
+        );
+        $this->assertEquals(0, count(Building::all()));
+    }
+    public function test_should_create_a_bathroom_paginator(): void
+    {
+        $building = new Building([
+        'name' => 'H',
+        'n_floors' => 3
+        ]);
+        $building->save();
+
+        $this->assertEquals(
+            0,
+            count(
+                $building->getBathroomPaginator(1, 10, 'route')->registers()
+            )
+        );
+
+        $bathroom = new Bathroom([
+        'building_id' => 1,
+        'floor' => 0
+        ]);
+        $bathroom->save();
+
+        $this->assertEquals(
+            1,
+            count(
+                $building->getBathroomPaginator(1, 10, 'route')->registers()
+            )
+        );
+    }
+}
