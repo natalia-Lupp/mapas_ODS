c3038eb9a8eedcb7fec3f134357409d9e16d10b0
natalia-Lupp <nata.lupp@gmail.com>
2025-10-16 20:52:28 -0300

    rebase: puxa as novidades da development para usar os populates da entrega 4.2

---
+++ app/Controllers/TemporariaController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Http\Controllers\Controller;
+use Lib\Authentication\Auth;
+
+class TemporariaController extends Controller
+{
+    public function navbarADM()
+    {
+        $this->render('components/navbar.admin', ['title' => '']);
+        //require_once __DIR__ . '/../views/components/navbar.admin.phtml';
+    }
+
+    public function sidenavADM()
+    {
+        $this->render('components/sidenav.admin', ['title' => '']);
+    }
+
+    public function navbarUser()
+    {
+        $this->render('components/navbar.user', ['title' => '']);
+    }
+
+    public function sidebarUser()
+    {
+        $this->render('components/sidenav.user', ['title' => '']);
+    }
+
+    public function dashboardADM()
+    {
+        $this->render('admin/dashbord.adm', ['title' => '']);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    public function sidenavADM()
+    // {
+    //    $is_admin = Auth::user()->hasRule('admin');
+    // Inclui a view do componente
+    //  $title = '';
+    //$this->render('components/sidenav.adm', compact('title', 'is_admin'));
+    //require_once __DIR__ . '/../views/components/navbar.admin.phtml';
+    //}
+}
