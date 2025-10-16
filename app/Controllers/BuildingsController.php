1db44478f99fc00993dce7c4aa78261cc88026b7
Gabriel Ramos <gcrepho@gmail.com>
2025-10-16 19:51:20 -0300

    feat: create BuildingsController

---
+++ app/Controllers/BuildingsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Http\Controllers\Controller;
+use App\Models\Building;
+use Core\Http\Request;
+use Lib\FlashMessage;
+
+class BuildingController extends Controller
+{
+    public function index(Request $request): void
+    {
+        $paginator = $this->current_user->buildings()->paginate(page: $request->getParam('page', 1));
+        $buildings = $paginator->registers();
+
+        $title = 'Prédios Registrados';
+
+        if ($request->acceptJson()) {
+            $this->renderJson('buildings/index', compact('paginator', 'buildings', 'title'));
+        } else {
+            $this->render('buildings/index', compact('paginator', 'buildings', 'title'));
+        }
+    }
+
+    public function new(): void
+    {
+        $building = $this->current_user->buildings()->new();
+
+        $title = 'Novo Prédio';
+        $this->render('buildings/new', compact('building', 'title'));
+    }
+
+    public function create(Request $request): void
+    {
+        $params = $request->getParams();
+        $building = $this->current_user->buildings()->new($params['building']);
+
+        if ($building->save()) {
+            FlashMessage::success('Prédio registrado com sucesso!!');
+            $this->redirectTo(route('buildings.index'));
+        } else {
+            FlashMessage::danger('Por favor verifique novamente os dados enviados! Cadastro não realizado.');
+            $title = 'Novo Prédio';
+            $this->render('buildings/new', compact('building', 'title'));
+        }
+    }
+
+    public function show(Request $request): void
+    {
+        $params = $request->getParams();
+
+        $building = $this->current_user->buildings()->findById($params['id']);
+
+        $title = "Prédio: {$building->name}";
+        $this->render('buildings/show', compact('building', 'title'));
+    }
+
+    public function edit(Request $request): void
+    {
+        $params = $request->getParams();
+        $building = $this->current_user->buildings()->findById($params['id']);
+
+        $title = "Editar Prédio: {$building->name}";
+        $this->render('buildings/edit', compact('building', 'title'));
+    }
+
+    public function update(Request $request): void
+    {
+        $id = $request->getParam('id');
+        $params = $request->getParam('buildings');
+
+        $building = $this->current_user->buildings()->findById($id);
+        $building->title = $params['title'];
+        $building->name = $params['name'];
+        $building->n_floors = $params['n_floors'];
+
+        if ($building->save()) {
+            FlashMessage::success("Predio {$building->name} atualizado com sucesso!");
+            $this->redirectTo(route('buildings.index'));
+        } else {
+            FlashMessage::danger('Por favor verifique novamente os dados enviados! Cadastro não foi editado.');
+            $title = "Editar Prédio: {$building->name}";
+            $this->render('buildings/edit', compact('buildings', 'title'));
+        }
+    }
+
+    public function destroy(Request $request): void
+    {
+        $params = $request->getParams();
+
+        $building = $this->current_user->problems()->findById($params['id']);
+        $building->destroy();
+
+        FlashMessage::success('Prédio removido com sucesso!');
+        $this->redirectTo(route('buildings.index'));
+    }
+}
