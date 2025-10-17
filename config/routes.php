f189aaae241d6fd74aae079ccc2d0d49ff2eb742
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-10-17 18:28:50 -0300

    fix: modifica config/routes.php - corris o nome de um contoler nomeado no commite anterior - comenta a adição de um controller não implenetado

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
 
 use App\Controllers\HomeController;
 use App\Controllers\AuthController;
-use App\Controllers\BuildingsController;
+use App\Controllers\BuildingController;
 use Core\Router\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -20,22 +20,22 @@ Route::middleware('auth')->group(function () {
         Route::get('/admin', [HomeController::class, 'dashboardAdmin'])->name('dashboard.admin');
 
     // Buildings ----------------------------------------------------------------------------
-        Route::get('/buildings', [BuildingsController::class, 'index'])->name('buildings.index');
+        Route::get('/buildings', [BuildingController::class, 'index'])->name('buildings.index');
 
         // Create Building
-        Route::get('/buildings/new', [ProblemsController::class, 'new'])->name('problems.new');
-        Route::post('/buildings', [BuildingsController::class, 'create'])->name('buildings.create');
+        //Route::get('/buildings/new', [ProblemsController::class, 'new'])->name('problems.new');
+        Route::post('/buildings', [BuildingController::class, 'create'])->name('buildings.create');
 
         // Retrieve Building
-        Route::get('/buildings/page/{page}', [BuildingsController::class, 'index'])->name('buildings.paginate');
-        Route::get('/buildings/{id}', [BuildingsController::class, 'show'])->name('buildings.show');
+        Route::get('/buildings/page/{page}', [BuildingController::class, 'index'])->name('buildings.paginate');
+        Route::get('/buildings/{id}', [BuildingController::class, 'show'])->name('buildings.show');
 
         // Update Building
-        Route::get('/buildings/{id}/edit', [BuildingsController::class, 'edit'])->name('buildings.edit');
-        Route::put('/buildings/{id}', [BuildingsController::class, 'update'])->name('buildings.update');
+        Route::get('/buildings/{id}/edit', [BuildingController::class, 'edit'])->name('buildings.edit');
+        Route::put('/buildings/{id}', [BuildingController::class, 'update'])->name('buildings.update');
 
         // Delete Building
-        Route::delete('/buildings/{id}', [BuildingsController::class, 'destroy'])->name('buildings.destroy');
+        Route::delete('/buildings/{id}', [BuildingController::class, 'destroy'])->name('buildings.destroy');
     });
 
     // User Routes
