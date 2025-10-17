0990c9bff460679dfb48eec0f8852db5e92888d5
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-10-17 17:14:00 -0300

    feature: adiciona populates para: - buildings - bathrooms - bathroom_item_types

--- database/Populate/populate.php
+++ database/Populate/populate.php
@@ -2,13 +2,19 @@
 
 require __DIR__ . '/../../config/bootstrap.php';
 
+use App\Models\Bathroom;
 use Core\Database\Database;
 use Database\Populate\UserPopulate;
 use Database\Populate\UserRulePopulate;
 use Database\Populate\AccountRulePopulate;
+use Database\Populate\BathroomItemTypePopulate;
+use Database\Populate\BuildingPopulate;
 
 Database::create();
 Database::migrate();
 UserPopulate::populate();
 UserRulePopulate::populate();
 AccountRulePopulate::populate();
+BuildingPopulate::populate();
+BathroomPopulate::populate();
+BathroomItemTypePopulate::populate();
