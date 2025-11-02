e89a9b78251a0612cd22d6f94fd484dab2c20bef
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-11-02 11:56:02 -0300

    fix: corrige ordem das estruturasde de controle em core/Router/Router::getRoutePathByName

--- core/Router/Router.php
+++ core/Router/Router.php
@@ -53,18 +53,18 @@ class Router
      */
     public function getRoutePathByName(string $name, array $params = []): string
     {
-        foreach ($this->routes as $route) {
-            if (strpos($route->getName(), '/') === false) {
-              if ($route->getName() === $name) {
-                  $routePath = $route->getUri();
-                  $routePath = $this->replaceRouteParams($routePath, $params);
-                  $routePath = $this->appendQueryParams($routePath, $params);
+        if (strpos($name, '/') === false) {
+            foreach ($this->routes as $route) {
+                if ($route->getName() === $name) {
+                    $routePath = $route->getUri();
+                    $routePath = $this->replaceRouteParams($routePath, $params);
+                    $routePath = $this->appendQueryParams($routePath, $params);
 
-                  return $routePath;
-              }
-            } else {
-              return $this->appendQueryParams($name, $params);
+                    return $routePath;
+                }
             }
+        } else {
+            return $this->appendQueryParams($name, $params);
         }
 
         throw new Exception("Route with name $name not found", 500);
