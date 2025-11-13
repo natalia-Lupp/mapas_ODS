09292539a25d40eaa5d37c852c59f9d028cd879a
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-11-13 20:08:54 -0300

    fix: decrição: - recoloca anotrações de tipo em app/Controllers/Admins/BathroomImagesController.php - move validações e menbros de app/Models/BathroomImage.php para app/Models/ImageModel.php

--- app/Controllers/Admins/BathroomImagesController.php
+++ app/Controllers/Admins/BathroomImagesController.php
@@ -43,48 +43,58 @@ class BathroomImagesController extends Controller
         $this->redirectBack();
     }
 
-   public function destroy(Request $request): void
-{
-    $building_id = $request->getParam('building_id');
-    $bathroom_id = $request->getParam('bathroom_id');
-    $image_id = $request->getParam('image_id');
+    public function destroy(Request $request): void
+    {
+        $building_id = $request->getParam('building_id');
+        $bathroom_id = $request->getParam('bathroom_id');
+        $image_id = $request->getParam('image_id');
 
-    $building = Building::findById($building_id);
-    if (!$building) {
-        FlashMessage::danger('Prédio não encontrado!');
-        $this->redirectTo(route('admin.buildings.index'));
-        return;
-    }
 
-    $bathroom = $building->bathrooms()->findById($bathroom_id);
-    if (!$bathroom) {
-        FlashMessage::danger('Banheiro não encontrado!');
-        $this->redirectTo(route('admin.buildings.bathrooms.index', [
+     /**
+      * @var Building | null $building
+      */
+        $building = Building::findById($building_id);
+        if (!$building) {
+              FlashMessage::danger('Prédio não encontrado!');
+              $this->redirectTo(route('admin.buildings.index'));
+              return;
+        }
+
+     /**
+      * @var Bathroom | null $bathroom
+      */
+        $bathroom = $building->bathrooms()->findById($bathroom_id);
+        if (!$bathroom) {
+             FlashMessage::danger('Banheiro não encontrado!');
+             $this->redirectTo(route('admin.buildings.bathrooms.index', [
             'building_id' => $building_id
-        ]));
-        return;
-    }
+             ]));
+             return;
+        }
 
-    $image = $bathroom->images()->findById($image_id);
-    if (!$image) {
-        FlashMessage::danger('Imagem não encontrada!');
-        $this->redirectTo(route('admin.buildings.bathrooms.show', [
+
+     /**
+      * @var ImageModel | null $image
+      */
+        $image = $bathroom->images()->findById($image_id);
+        if (!$image) {
+            FlashMessage::danger('Imagem não encontrada!');
+            $this->redirectTo(route('admin.buildings.bathrooms.show', [
             'building_id' => $building_id,
             'id' => $bathroom_id
-        ]));
-        return;
-    }
+            ]));
+            return;
+        }
 
-    if ($image->imageService()->deleteImage()) {
-        FlashMessage::success('Imagem removida com sucesso!');
-    } else {
-        FlashMessage::danger('Falha ao remover a imagem!');
-    }
+        if ($image->imageService()->deleteImage()) {
+            FlashMessage::success('Imagem removida com sucesso!');
+        } else {
+            FlashMessage::danger('Falha ao remover a imagem!');
+        }
 
-    $this->redirectTo(route('admin.buildings.bathrooms.show', [
+        $this->redirectTo(route('admin.buildings.bathrooms.show', [
         'building_id' => $building_id,
         'id' => $bathroom_id
-    ]));
-}
-
+        ]));
+    }
 }
--- app/Models/BathroomImage.php
+++ app/Models/BathroomImage.php
@@ -18,13 +18,8 @@ class BathroomImage extends ImageModel
     protected static string $table = 'bathroom_images';
     protected static array $columns = [
         'bathroom_id',
-        static::IMAGE_FILD_NAME
+        self::IMAGE_FILD_NAME
     ];
-    public ?string $image_name;
-    public ?string $image_type;
-    public ?int $image_size;
-    public ?string $image_temp_name;
-    public const int MAX_IMAGE_ACEPTED_SIZE = (40 * 1048576); // 2MB
 
 
     public function validates(): void
@@ -34,18 +29,7 @@ class BathroomImage extends ImageModel
         Validations::isInt('bathroom_id', $this);
         Validations::inRange('bathroom_id', 1, PHP_INT_MAX, $this);
         Validations::isIdFrom('bathroom_id', $this, Bathroom::class);
-
-        Validations::notEmpty('image_name', $this);
-        Validations::isString('image_name', $this);
-
         // 🔹 Validar tipo e tamanho da imagem (se existirem)
-        if (isset($this->image_size) && $this->image_size > self::MAX_IMAGE_ACEPTED_SIZE) {
-            $this->addError('image_name', 'A imagem excede o tamanho máximo permitido de 40 MB.');
-        }
-
-        if (isset($this->image_type) && !in_array($this->image_type, ['image/jpg', 'image/png', 'image/jpeg'])) {
-            $this->addError('image_name', 'Formato de imagem inválido. Use JPG, PNG ou JPEG.');
-        }
     }
 
 
