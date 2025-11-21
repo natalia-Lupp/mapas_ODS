6fc6d9fb34262847049cea3e73156a8fafaf82a3
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-11-20 21:00:12 -0300

    fix: cria dicisorias em App\Models\dCacheableModel::$cache para atender multiplas sub classes sem comflitos de id

--- app/Models/IdCacheableModel.php
+++ app/Models/IdCacheableModel.php
@@ -6,16 +6,22 @@ use Core\Database\ActiveRecord\Model;
 
 abstract class IdCacheableModel extends Model
 {
-
     /**
-     * @var array<int, static |null> $cache
+     * @var array<string, array<int, static |null> | null> $cache
      */
     protected static array $cache = [];
 
+    /**
+     * @return static | null
+     */
     public static function findById(int $id): static|null
     {
-        return isset(self::$cache[$id])
-            ? self::$cache[$id]
-            : (self::$cache[$id] = parent::findById($id));
+        if (!isset(static::$cache[static::class])) {
+            $cache[static::class] = [];
+        }
+
+        return isset(static::$cache[static::class][$id])
+            ? static::$cache[static::class][$id]
+            : (static::$cache[static::class][$id] = parent::findById($id));
     }
 }
