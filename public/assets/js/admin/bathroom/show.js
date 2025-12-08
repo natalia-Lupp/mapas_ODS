9dbc2180ab65aa287a87d0090ee11335bec525ee
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2025-12-07 22:18:09 -0300

    featury: adiciona uma tabela con conteudo caregado via ajax. files: - app/views/admin/bathrooms/show.phtml - public/assets/js/admin/bathroom/show.js

--- public/assets/js/admin/bathroom/show.js
+++ public/assets/js/admin/bathroom/show.js
@@ -1,9 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
+function capitalizeFirstLetter(str) {
+  if (!str) {
+    return ""; // Handle empty or null strings
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+document.addEventListener("DOMContentLoaded", async () => {
   const showFormButton = document.getElementById("showFormButton");
   const formContainer = document.getElementById("formContainer");
-
+  const consumptions = document.getElementById('consumptions');
   // --- LÓGICA DO MODAL DE EXCLUSÃO ---
   let imageFormToDelete = null;
+
+  let email = sessionStorage.getItem('email');
+  let password = sessionStorage.getItem('password');
+
+  if (email && password) {
+    fetch(
+      `/api/buildings/${consumptions.getAttribute('building')}/bathrooms/${consumptions.getAttribute('bathroom')}/consumptions`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Basic ${btoa(email+':'+password)}`
+        }
+      }
+    ).then( async (response) => {
+      data = JSON.parse(await response.text());
+      if (data.length > 0) {
+
+        data.forEach((o) =>{
+          consumptions.innerHTML += `
+            <tr>
+              <td>${capitalizeFirstLetter(o.name)}</td>
+              <td>${o.quantity}L</td>
+              <td>${o.date}</td>
+            </tr>
+          `
+        });
+      } else {
+        consumptions.innerHTML += `<tr>Nenhum registro de comsumo.</tr>`;
+      }
+    }).catch((e) => consumptions.innerHTML += e  ?? `<tr>Nenhum registro de comsumo.</tr>`)
+  }
 
   document.querySelectorAll(".btn-delete-image").forEach((btn) => {
     btn.addEventListener("click", () => {
